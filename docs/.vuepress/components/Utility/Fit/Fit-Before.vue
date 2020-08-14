348d3def391d6b098838c855c3e9482f862e2b6d
Harwin Borger <harwin@studioborger.nl>
2020-08-14 23:18:54 +0200

    Add fit documentation

---
+++ docs/.vuepress/components/Utility/Fit/Fit-Before.vue
@@ -0,0 +1,32 @@
+<template>
+	<Example >
+		<div class="example-overflow bg-padding  example-fit-padding">
+			<div class="example-overflow  bg-white-o-20">
+				<div class="text-preset-xlarge">xLarge</div>
+				<div class="text-preset-default">Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere
+					consectetur est at lobortis. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis
+					euismod.
+				</div>
+			</div>
+		</div>
+	</Example>
+</template>
+<script>
+	import Example from '../../Example'
+
+	export default {
+		name: 'TextPreset',
+		props: ['content'],
+		components: {
+			Example
+		}
+	}
+</script>
+<style lang="scss">
+	@import "../../../scss/init";
+
+	.example-fit-padding {
+		@include padding(2, 2, 2, 2);
+	}
+</style>
--- docs/utility-classes/fit.md
+++ docs/utility-classes/fit.md
@@ -1,5 +1,26 @@
 # Fit
+Fit removes white space from the line-height so it fit's perfectly to it's container. 
 
+::: warning Please notice:
+Currently this only works combined with `.align-baseline` and `.align-capline` classes. The standalone .fit class has yet to come. 
+:::
+
+## Introduction
+
+### Benefits of using .fit classs
+- No longer you need to give your padding strange values to compensate for line-heights. 
+
+## Example
+#### Before .align-baseline and .fit
+<Utility-Fit-Fit-Before />
+
+#### After .align-baseline and .fit
+<Utility-Fit-Fit-After />
+
+## Requirements
+- Use of text-presets is required in order to have the Fit class work
+
+## Specifications
 **Classname:** `.fit`
 
 > We will add documentation soon
