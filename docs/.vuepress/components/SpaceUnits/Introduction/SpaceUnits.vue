440aa05ba0809fed1e3fbb0d6c11ca0d6ae20440
Harwin Borger <harwin@studioborger.nl>
2020-08-15 12:10:36 +0200

    Improve space unit example

--- docs/.vuepress/components/SpaceUnits/Introduction/SpaceUnits.vue
+++ docs/.vuepress/components/SpaceUnits/Introduction/SpaceUnits.vue
@@ -1,16 +1,18 @@
 <template>
-	<Example raster>
-		<div class="space-unit-example trim align-baseline">
-			<p class="text-preset-default">
+	<Example raster class="align-baseline">
+		<div class="space-unit-example trim ">
+			<p>
 				<span class="inline-su-1"></span> The red block is 1 space unit height and width.
 			</p>
 
-			<p class="text-preset-default">It is in a container with 2 space units padding.</p>
+			<p>It is in a container with 2 space units padding.</p>
 
-			<p class="text-preset-default">And each paragraph has a lineheight of 2 space units and a vertical margin of
+			<p>And each paragraph has a lineheight of 2 space units and a vertical margin of
 				1 space unit</p>
 
-			<p class="text-preset-default">Each line is aligned on the baseline</p>
+			<p>Each line is aligned on the baseline</p>
+
+			<p>When you scale the window, the size of the space units will change</p>
 		</div>
 	</Example>
 </template>
@@ -33,6 +35,7 @@
 		@include min-height(10);
 
 		p {
+			@include text-preset(default);
 			@include margin-vertical(1);
 		}
 	}
